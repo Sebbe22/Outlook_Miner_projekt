99431f2815d2f9ac62bbb48a013a5709e4d832cb
Sebbe22 <seba124m@edu.zealand.dk>
2023-10-27 12:20:29 +0200

    lavede regex for fjernelse af links bredere og lavede unittests til metoden

--- OutlookMiner/Services/ICleanService.cs
+++ OutlookMiner/Services/ICleanService.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace OutlookMiner.Services
 {
     public interface ICleanService
     {
-
+        string RemoveLinksFromEmailString(string emailString);
     }
+
+    public class CleanService : ICleanService
+    {
+        public string RemoveLinksFromEmailString(string emailString)
+        {
+            // (?:<https?|<www|\bhttps?|\bwww)\S*?(?=\s|>)(?:>)?
+            string pattern = @"(<\w+:\/\/\S+>|www\.\w+\.\w+(?:\.\w+)?|\w+\.\w+\.\w+(?:\.\w+)?|\w+:\/\/\S+|<\w+\.\w+\.\w+(?:\.\w+)?>)";
+
+            string result = Regex.Replace(emailString, pattern, string.Empty);
+
+            return result;
+        }
+
+        public string RemoveEmailsFromEmailString(string emailString)
+        {
+            string pattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}";
+
+            string result = Regex.Replace(emailString, pattern, string.Empty);
+
+            return result;
+        }
+
+        public string RemoveSenderNameFromEmail(string senderName, string emailString)
+        {
+            return "";
+        }
+    }
+
 }
