f23b297843eacd96ba456d1ecdf8017e6b74bd8b
Sebbe22 <seba124m@edu.zealand.dk>
2023-11-08 14:02:15 +0100

    fixed bug when email body is null

--- OutlookMiner/Services/ILoadService.cs
+++ OutlookMiner/Services/ILoadService.cs
@@ -65,7 +65,7 @@ namespace OutlookMiner.Services
                             {
                                 email.recipients.Add(oMsg.Recipients[i].Name);
                             }
-                            email.body.ToLower();
+                            email.body = email.body.ToLower();
                             result.Add(email);
                         }
                     }
